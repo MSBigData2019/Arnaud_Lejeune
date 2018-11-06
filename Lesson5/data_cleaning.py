467c7746f3929c6a609aa060db6e4ae77bac3e2e
EMBSKD <arnaudlejeune44@gmail.com>
2018-11-06 14:45:23 +0100

    V2, ajout de plusieurs courbes et enrichissement des données par des données démographiques

--- Lesson5/data_cleaning.py
+++ Lesson5/data_cleaning.py
@@ -2,40 +2,49 @@ import pandas as pd
 import numpy as np
 import re
 
-#  --- Import des 2 feuilles csv ---
-spe = pd.read_csv('Specialistes.csv', sep = ",", header = 0)
-gen = pd.read_csv('Generalistes.csv', sep = ",", header = 0)
+def DataMedecin():
+	#  --- Import des 2 feuilles csv ---
+	spe = pd.read_csv('Specialistes.csv', sep = ",", header = 0)
+	gen = pd.read_csv('Generalistes.csv', sep = ",", header = 0)
 
-	# Renommage des premieres colonnes de chaque df
-spe = spe.rename(columns={'Spécialistes': 'SPECIALITE'})
-gen = gen.rename(columns={'Généralistes et compétences MEP': 'SPECIALITE'})
-	
-	# Fusion des 2 df
-data = spe.append(gen)
+		# Renommage des premieres colonnes de chaque df
+	spe = spe.rename(columns={'Spécialistes': 'SPECIALITE'})
+	gen = gen.rename(columns={'Généralistes et compétences MEP': 'SPECIALITE'})
+		
+		# Fusion des 2 df
+	data = spe.append(gen)
 
-print(data.shape)
+	print(data.shape)
 
-#  --- Cleaning du DataFrame---
-data_cleaned = data.copy()
+	#  --- Cleaning du DataFrame---
+	data_cleaned = data.copy()
 
-	# Valeurs non renseignees
-data_cleaned = data_cleaned.replace('nc', np.NaN)
-	# Lignes TOTAL par specialitees
-data_cleaned['SPECIALITE'] = data_cleaned['SPECIALITE'].replace(to_replace = r'TOTAL(.*)', value = np.NaN, regex = True)
-	# Lignes TOTAL par departement
-data_cleaned['DEPARTEMENT'] = data_cleaned['DEPARTEMENT'].replace(to_replace = r'TOTAL(.*)', value = np.NaN, regex = True)
-	# Lignes aux effectifs = 0
-data_cleaned['EFFECTIFS'] = data_cleaned['EFFECTIFS'].replace(to_replace = '0', value = np.NaN)
-	# Suppression des lignes 'selectionnes' precedemment
-data_cleaned = data_cleaned.dropna(axis = 0, how = 'any')
+		# Valeurs non renseignees
+	data_cleaned = data_cleaned.replace('nc', np.NaN)
+		# Lignes TOTAL par specialitees
+	data_cleaned['SPECIALITE'] = data_cleaned['SPECIALITE'].replace(to_replace = r'TOTAL(.*)', value = np.NaN, regex = True)
+		# Lignes TOTAL par departement
+	data_cleaned['DEPARTEMENT'] = data_cleaned['DEPARTEMENT'].replace(to_replace = r'TOTAL(.*)', value = np.NaN, regex = True)
+		# Lignes aux effectifs = 0
+	data_cleaned['EFFECTIFS'] = data_cleaned['EFFECTIFS'].replace(to_replace = '0', value = np.NaN)
+		# Suppression des lignes 'selectionnes' precedemment
+	data_cleaned = data_cleaned.dropna(axis = 0, how = 'any')
 
-print(data_cleaned.shape)
+	print(data_cleaned.shape)
 
 
-# --- Renommage des colonnes utilisees ensuite, puis passage en int---
-data_cleaned = data_cleaned.rename(columns={'HONORAIRES SANS DEPASSEMENT (Euros)': 'HONORAIRES', 'DEPASSEMENTS (Euros)': 'DEPASSEMENTS'})
-data_cleaned['EFFECTIFS'] = pd.to_numeric(data_cleaned['EFFECTIFS'].replace({r'\s': ''}, regex=True))
-data_cleaned['HONORAIRES'] = pd.to_numeric(data_cleaned['HONORAIRES'].replace({r'\s': ''}, regex=True))
-data_cleaned['DEPASSEMENTS'] = pd.to_numeric(data_cleaned['DEPASSEMENTS'].replace({r'\s': ''}, regex=True))
-# --- Export ---
-data_cleaned.to_csv('cleaned_data.csv')
+	# --- Renommage des colonnes utilisees ensuite, puis passage en int---
+	data_cleaned = data_cleaned.rename(columns={'HONORAIRES SANS DEPASSEMENT (Euros)': 'HONORAIRES', 'DEPASSEMENTS (Euros)': 'DEPASSEMENTS'})
+	data_cleaned['EFFECTIFS'] = pd.to_numeric(data_cleaned['EFFECTIFS'].replace({r'\s': ''}, regex=True))
+	data_cleaned['HONORAIRES'] = pd.to_numeric(data_cleaned['HONORAIRES'].replace({r'\s': ''}, regex=True))
+	data_cleaned['DEPASSEMENTS'] = pd.to_numeric(data_cleaned['DEPASSEMENTS'].replace({r'\s': ''}, regex=True))
+	# --- Export ---
+	data_cleaned.to_csv('cleaned_data.csv')
+
+def DataPopulation() :
+	temp = pd.read_csv('population.csv', sep = ",", header = None)
+	population = pd.DataFrame(columns = ['DEPARTEMENT', 'POPULATION'])
+	population['DEPARTEMENT'] = temp[0] + "- " + temp[1]
+	population['POPULATION'] = temp[2]
+	population['POPULATION'] = pd.to_numeric(population['POPULATION'].replace({r'\s': ''}, regex=True))
+	population.to_csv('cleaned_population.csv')
--- Lesson5/data_processing.py
+++ Lesson5/data_processing.py
@@ -10,14 +10,30 @@ col = ['EFFECTIFS','HONORAIRES','DEPASSEMENTS']
 departement = data.groupby(['DEPARTEMENT'])[col].sum()
 specialites = data.groupby(['SPECIALITE'])[col].sum()
 
-departement['RATIO'] = departement['DEPASSEMENTS'] / (departement['DEPASSEMENTS'] + departement['HONORAIRES'])
-specialites['RATIO'] = specialites['DEPASSEMENTS'] / (specialites['DEPASSEMENTS'] + specialites['HONORAIRES'])
+departement['RATIO_DEPASSEMENT'] = departement['DEPASSEMENTS'] / (departement['DEPASSEMENTS'] + departement['HONORAIRES'])
+specialites['RATIO_DEPASSEMENT'] = specialites['DEPASSEMENTS'] / (specialites['DEPASSEMENTS'] + specialites['HONORAIRES'])
+
+population = pd.read_csv('cleaned_population.csv', index_col = 0)
+
+departement = pd.merge(departement, population, on = 'DEPARTEMENT', left_index=True, right_index=False)
+
+departement['RATIO_EFFECTIF_POP'] = departement['EFFECTIFS'] / departement['POPULATION']
+
+def plot_1():
+	ax1 = departement.plot(x = 'EFFECTIFS', y = 'RATIO_DEPASSEMENT', marker = 'o', linewidth= 0 ,  color = 'cornflowerblue')
+	ax1.set_title("% de depassement fonction de l'effectif du departement")
+
+	ax2 = specialites.plot(x = 'EFFECTIFS', y = 'RATIO_DEPASSEMENT', marker = 'o', linewidth= 0 ,  color = 'firebrick')
+	ax2.set_title("% de depassement fonction de l'effectif de la specialites")
+
+	plt.show()
 
 
-ax1 = departement.plot(x = 'EFFECTIFS', y = 'RATIO', marker = 'o', linewidth= 0 ,  color = 'cornflowerblue')
-ax1.set_title("% de depassement fonction de l'effectif du departement")
+def plot_2():
+	ax3 = departement.plot(x = 'POPULATION', y = 'RATIO_DEPASSEMENT', marker = 'o', linewidth= 0 ,  color = 'forestgreen')
+	plt.show()
 
-ax2 = specialites.plot(x = 'EFFECTIFS', y = 'RATIO', marker = 'o', linewidth= 0 ,  color = 'firebrick')
-ax2.set_title("% de depassement fonction de l'effectif de la specialites")
 
-plt.show()
+def plot_3():
+	ax4 = departement.plot(x = 'RATIO_EFFECTIF_POP', y = 'RATIO_DEPASSEMENT', marker = 'o', linewidth= 0 ,  color = 'darkorange')
+	plt.show()
